a5271f316c4360531a3cef03beca49d069e5d8b0
Mike Jackson <perigran_falcon@yahoo.com>
2023-03-12 15:27:20 -0600

    Add files via upload

---
+++ Constant/Enums.cs
@@ -0,0 +1,1 @@
+﻿enum Days { Sat, Sun, Mon, Tue, Wed, Thu, Fri }
---
+++ Constant/Program.cs
@@ -0,0 +1,19 @@
+﻿// Constant Project
+
+Console.Title = "Constant";
+
+const double pi = 3.14159265358979;
+var daysType = typeof(Days);
+string name = Enum.GetName(daysType, 1);
+bool flag = Enum.IsDefined(daysType, "Mon");
+
+Console.WriteLine("Pi Type: " + pi.GetType());
+Console.WriteLine("Circumference: " + (pi * 3) + "\n");
+Console.WriteLine("First Name: " + Days.Sat);
+Console.WriteLine("1st Index: " + (int)Days.Sat + "\n");
+Console.WriteLine("2nd Index: " + name);
+Console.WriteLine("Contains Mon?: " + flag);
+
+Console.ReadKey();
+
+
